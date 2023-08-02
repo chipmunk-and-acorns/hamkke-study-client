ef34871ff71375a5909e7ae08b7534da298edf04
jiye-7 <jy522hi@gmail.com>
2023-08-02 23:31:16 +0900

    #1 tanstack-query, devtools setting

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { RecoilRoot } from 'recoil';
 import { RouterProvider } from 'react-router-dom';
 
 import { router } from './routes/router.tsx';
 
+const queryClient = new QueryClient();
+
 function App() {
   return (
-    <RecoilRoot>
-      <RouterProvider router={router} />
-    </RecoilRoot>
+    <QueryClientProvider client={queryClient}>
+      <RecoilRoot>
+        <RouterProvider router={router} />
+      </RecoilRoot>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 }
 
