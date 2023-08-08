830cf0c978c6559cd1c81c3d07ba045d90f9952e
jiye-7 <jy522hi@gmail.com>
2023-08-08 19:20:01 +0900

    #5 회원가입 페이지에서 받을 데이터 정리

---
+++ src/pages/User/SignUp.tsx
@@ -0,0 +1,22 @@
+/**
+ * 일반 회원가입
+ * - [ ] email
+ * - [ ] password
+ * - [ ] nickname
+ * - [ ] profileImage(optional)를 입력받는다.
+ *
+ * 구글 OAuth 가입
+ * - [ ] email disable 처리
+ * - [ ] nickname 수정 가능
+ * - [ ] profileImage 기존 소셜 이미지 사용 변경 가능
+ *
+ * 회원 가입 성공 후 로직
+ * - 로그인 처리한 후 페이지 이동
+ *  - 이전 페이지로 이동 (없다면 메인 페이지로 라우팅)
+ */
+
+const SignUp = () => {
+  return <div>SignUp</div>;
+};
+
+export default SignUp;
