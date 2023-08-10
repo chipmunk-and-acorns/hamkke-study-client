36b260debe04925945dd8fd1bb0a96e78ed41ed0
jiye-7 <jy522hi@gmail.com>
2023-08-10 21:32:52 +0900

    #5 회원가입 페이지 레이아웃

--- src/pages/User/SignUp.tsx
+++ src/pages/User/SignUp.tsx
@@ -1,4 +1,7 @@
-import { Container, Box, useTheme } from '@mui/material';
+import { Container, Box, Typography, Button } from '@mui/material';
+
+import CustomInput from '../../components/CustomInput';
+import { useTheme } from '@mui/material';
 
 /**
  * 일반 회원가입
@@ -20,9 +23,59 @@ import { Container, Box, useTheme } from '@mui/material';
 const SignUp = () => {
   const theme = useTheme();
 
+  const ContainerStyle = {
+    display: 'flex',
+    flexDirection: 'column',
+    marginTop: '7rem',
+    paddingY: '4rem',
+    backgroundColor: theme.customPalette.grey[50],
+    border: `1px solid ${theme.customPalette.grey[100]}`,
+    borderRadius: '0.5rem',
+
+    '& > div': {
+      paddingX: '5rem',
+      '> div': {
+        paddingBottom: '1.3rem',
+      },
+    },
+
+    'button:nth-child(1)': {
+      marginTop: '3rem',
+      width: '100%',
+    },
+  };
+
   return (
-    <Container typeof="div" maxWidth="sm" sx={{ bgcolor: theme.palette.primary.dark }}>
-      SignUp
+    <Container typeof="div" maxWidth="sm" sx={ContainerStyle}>
+      <Typography variant="h4" component="h4" sx={{ paddingBottom: '3rem', textAlign: 'center' }}>
+        SignUp
+      </Typography>
+      <Box>
+        <CustomInput content="이메일" htmlFor="email" value="" inputId="email" />
+        <CustomInput content="비밀번호" htmlFor="password" value="" inputId="password" />
+        <CustomInput
+          content="비밀번호 확인"
+          htmlFor="passwordConfirm"
+          value=""
+          inputId="passwordConfirm"
+        />
+        <CustomInput content="닉네임" htmlFor="nickname" value="" inputId="nickname" />
+      </Box>
+      <Box
+        sx={{
+          display: 'flex',
+          justifyContent: 'right',
+          alignItems: 'center',
+        }}
+      >
+        <Typography component="span" variant="h6" sx={{ fontSize: '0.9rem' }}>
+          이미 회원이신가요?
+        </Typography>
+        <Button>로그인하기</Button>
+      </Box>
+      <Box>
+        <Button variant="contained">회원가입</Button>
+      </Box>
     </Container>
   );
 };
