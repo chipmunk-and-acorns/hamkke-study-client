9a99358d245c74ba42ba108e0f143eb05abefb27
jiye-7 <jy522hi@gmail.com>
2023-08-10 22:21:09 +0900

    #5 에러 페이지 추가

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -3,12 +3,13 @@ import { createBrowserRouter } from 'react-router-dom';
 import Root from './root';
 import Main from '../pages/Main';
 import SignUp from '../pages/User/SignUp';
+import ErrorPage from '../pages/ErrorPage';
 
 export const router = createBrowserRouter([
   {
     path: '/',
     element: <Root />,
-    errorElement: <div>Something Wrong ...</div>,
+    errorElement: <ErrorPage />,
     children: [
       {
         path: '/',
