21860a8b080f1a0a1dba95813095b04aed3679b0
jiye-7 <jy522hi@gmail.com>
2023-08-13 01:21:00 +0900

    #5 input component 추가수정
    - formik 관련 코드 추가
    - 스타일 코드 분리

---
+++ src/components/TextInput.tsx
@@ -0,0 +1,45 @@
+import { useField } from 'formik';
+import { Box, InputLabel, OutlinedInput, Typography } from '@mui/material';
+
+import theme from '../styles/theme';
+
+interface IProps {
+  label: string;
+  name: string;
+  type: string;
+}
+
+const TextInput = ({ label, ...props }: IProps) => {
+  const [filed, meta] = useField(props);
+
+  return (
+    <>
+      <Box sx={InputBoxStyle}>
+        <InputLabel children={label} htmlFor={props.name} variant="standard" />
+        <OutlinedInput {...filed} {...props} id={props.name} />
+      </Box>
+      <Box sx={ErrorBoxStyle}>
+        {meta.touched && meta.error && <Typography variant="caption">{meta.error}</Typography>}
+      </Box>
+    </>
+  );
+};
+
+const InputBoxStyle = {
+  label: {
+    fontSize: '1.1rem',
+    fontWeight: 'bold',
+  },
+  div: {
+    width: '100%',
+  },
+};
+
+const ErrorBoxStyle = {
+  height: '1rem',
+  mb: '0.7rem',
+  textAlign: 'center',
+  color: theme.customPalette.error.main,
+};
+
+export default TextInput;
