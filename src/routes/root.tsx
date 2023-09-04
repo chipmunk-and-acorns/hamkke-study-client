cb28c6aa8bc3db17742f26961be6e0c4e19f1462
jiye-7 <jy522hi@gmail.com>
2023-09-04 22:05:20 +0900

    #29 pathname enum으로 변경

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -2,12 +2,17 @@ import { Outlet, useLocation } from 'react-router-dom';
 
 import GNB from '../components/GNB/GlobalNavigationBar';
 
+enum PathName {
+  Register = '/register',
+  Login = '/login',
+}
+
 const Root = () => {
   const { pathname } = useLocation();
 
   return (
     <>
-      {pathname !== '/register' && pathname !== '/login' && <GNB />}
+      {pathname !== PathName.Register && pathname !== PathName.Login && <GNB />}
       <Outlet />
     </>
   );
