5bc535e62494af8ffc5a3e503cf05727160aa624
jiye-7 <jy522hi@gmail.com>
2023-09-05 21:32:26 +0900

    #30 이용 약관 모달 추가

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,32 +1,54 @@
+import { MouseEvent, useState } from 'react';
 import { Container, Box, Typography, List, ListItem, ListItemText } from '@mui/material';
+
 import { images } from '../../utils/importImageUrl';
 import theme from '../../styles/theme';
+import TermsOfUseModal from '../../pages/TermsOfUse';
+
+type TermsState = {
+  [key: string]: boolean;
+};
 
 const Footer = () => {
+  const [terms, setTerms] = useState<TermsState>({ termsOfUse: false, privacyPolicy: false });
+
+  const handleClick = (e: MouseEvent<HTMLElement>) => {
+    e.preventDefault();
+
+    if (e.currentTarget.id) {
+      setTerms({ ...terms, [e.currentTarget.id]: !terms[e.currentTarget.id] });
+    }
+  };
+
   return (
-    <Container sx={ContainerStyle}>
-      <footer>
-        <Box sx={LeftBoxStyle}>
-          <img src={images.logo} alt="main logo image" />
-          <Typography variant="subtitle2" component="p">
-            온라인/오프라인 스터디 모집을 위한 커뮤니티 서비스 함께입니다.
-          </Typography>
-        </Box>
-        <Box sx={RightBoxStyle}>
-          <List>
-            <ListItem>
-              <ListItemText>이용약관</ListItemText>
-            </ListItem>
-            <ListItem>
-              <ListItemText>개인정보 처리방침</ListItemText>
-            </ListItem>
-            <ListItem>
-              <ListItemText>서비스 소개</ListItemText>
-            </ListItem>
-          </List>
-        </Box>
-      </footer>
-    </Container>
+    <>
+      <Container sx={ContainerStyle}>
+        <footer>
+          <Box sx={LeftBoxStyle}>
+            <img src={images.logo} alt="main logo image" />
+            <Typography variant="subtitle2" component="p">
+              온라인/오프라인 스터디 모집을 위한 커뮤니티 서비스 함께입니다.
+            </Typography>
+          </Box>
+          <Box sx={RightBoxStyle}>
+            <List>
+              <ListItem id="termsOfUse" onClick={(e: MouseEvent<HTMLElement>) => handleClick(e)}>
+                <ListItemText>이용약관</ListItemText>
+              </ListItem>
+              <ListItem id="privacyPolicy" onClick={(e: MouseEvent<HTMLElement>) => handleClick(e)}>
+                <ListItemText>개인정보 처리방침</ListItemText>
+              </ListItem>
+              <ListItem>
+                <ListItemText>서비스 소개</ListItemText>
+              </ListItem>
+            </List>
+          </Box>
+        </footer>
+      </Container>
+      {terms.termsOfUse && (
+        <TermsOfUseModal handleClick={() => setTerms({ ...terms, termsOfUse: false })} />
+      )}
+    </>
   );
 };
 
@@ -59,13 +81,14 @@ const LeftBoxStyle = {
 };
 
 const RightBoxStyle = {
+  width: '35%',
   ul: {
     display: 'flex',
     alignItems: 'center',
     pt: '3rem',
 
     li: {
-      width: '150px',
+      textAlign: 'center',
       span: {
         color: theme.palette.grey[800],
         fontWeight: 'bold',
