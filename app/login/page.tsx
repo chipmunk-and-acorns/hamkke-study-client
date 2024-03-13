eaaf6140996613030f49ebc0ce42c60bca85dcaa
jiye-7 <jy522hi@gmail.com>
2024-03-13 22:08:19 +0900

    add login page
    - layout 작업

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,5 +1,80 @@
+"use client";
+
+import { FormEvent } from "react";
+import { LockRounded } from "@mui/icons-material";
+import {
+  Container,
+  Typography,
+  Box,
+  TextField,
+  Avatar,
+  Button,
+} from "@mui/material";
+
+import Copyright from "@/components/copyright/copyright";
+import Link from "next/link";
+
 const Login = () => {
-  return <div>Login</div>;
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+  };
+
+  return (
+    <Container component="main" maxWidth="xs">
+      <Box
+        sx={{
+          marginTop: 8,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Typography variant="h4" component="h1">
+          Login
+        </Typography>
+        <Avatar sx={{ m: 4, bgcolor: "primary.dark" }}>
+          <LockRounded />
+        </Avatar>
+        <Container>
+          <Box component="form" onSubmit={handleSubmit}>
+            <TextField
+              required
+              variant="standard"
+              id="email"
+              label="Email Address"
+              fullWidth
+              size="small"
+              margin="dense"
+            />
+            <TextField
+              required
+              variant="standard"
+              id="password"
+              label="Password"
+              fullWidth
+              size="small"
+              margin="dense"
+            />
+          </Box>
+          <Button variant="contained" fullWidth sx={{ my: 5 }}>
+            Login
+          </Button>
+          <Link href="/register">
+            <Typography
+              variant="subtitle2"
+              sx={{
+                color: "info.dark",
+                textAlign: "center",
+              }}
+            >
+              {"아직 회원이 아니신가요? 가입하기"}
+            </Typography>
+          </Link>
+          <Copyright sx={{ mt: 8 }} />
+        </Container>
+      </Box>
+    </Container>
+  );
 };
 
 export default Login;
---
+++ components/navigation/navigation.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import Image from "next/image";
+
+import styles from "./navigation.module.css";
+import hamkkeLogo from "@/public/assets/hamkke_logo.png";
+
+const MainNavigation = () => {
+  const pathname = usePathname();
+
+  return (
+    <header className={styles.header}>
+      <Link href="/">
+        <Image src={hamkkeLogo} alt="Going home to hamkke" />
+      </Link>
+      <nav className={styles.nav}>
+        <ul>
+          <li>
+            <Link
+              href="/login"
+              className={styles[`${pathname === "/login" && "active"}`]}
+            >
+              로그인
+            </Link>
+          </li>
+          <li>
+            <Link
+              href="/profile"
+              className={styles[`${pathname === "/profile" && "active"}`]}
+            >
+              마이 페이지
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default MainNavigation;
