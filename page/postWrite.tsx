433be19af10629b3735260cdb5d0af1f40d50be8
jiye-7 <jy522hi@gmail.com>
2024-03-19 03:37:41 +0900

    add post select, pre question
    - 글 전체 유효성 검증 추가하기
    - 서버 테스트 추가하기

--- page/postWrite.tsx
+++ page/postWrite.tsx
@@ -1,38 +1,80 @@
 "use client";
 
+import { useState } from "react";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { DemoItem } from "@mui/x-date-pickers/internals/demo";
 import { DatePicker } from "@mui/x-date-pickers/DatePicker";
 import Grid from "@mui/material/Unstable_Grid2";
-import { Box, Container, Divider, Typography, Checkbox } from "@mui/material";
+import NavigateNextRoundedIcon from "@mui/icons-material/NavigateNextRounded";
+import AddIcon from "@mui/icons-material/Add";
+import DeleteIcon from "@mui/icons-material/Delete";
+import CheckRoundedIcon from "@mui/icons-material/CheckRounded";
 import { Input } from "@mui/joy";
+import {
+  Box,
+  Container,
+  Divider,
+  Typography,
+  Radio,
+  Fab,
+  Tooltip,
+  Button,
+} from "@mui/material";
 
 import { position, postTypes, recruitCount, stacks } from "@/constants/data";
 import SelectBox from "@/components/select/selectBox";
 import QuillEditor from "@/components/quill/reactQuill";
-
-/**
- * 글 작성시 사용할 값들
- * 모집구분 (postType: 'project', 'study') -> 단일 선택
- * 모집 인원 (recruitCount) -> 1 ~ 10
- * 기술 스택 (stacks) -> 다중 선택
- * 모집 포지션 (positions) -> 다중 선택
- * 모집 마감일 (deadline) -> date
- *
- * 본문 내용에 진행방식, 기간 등 작성하는 것이 더 좋을 것 같음
- */
-
-const label = { inputProps: { "aria-label": "Checkbox demo" } };
+interface IPreQuestion {
+  value: string;
+  id: string;
+}
 
 const PostWritePage = () => {
+  const [selectedValue, setSelectedValue] = useState("");
+  const [preWriteQuestion, setPreWriteQuestion] = useState("");
+  const [preQuestionList, setPreQuestionList] = useState<IPreQuestion[]>([]);
+
+  // 참여 신청 여부 선택값 변경 함수
+  // TODO: 'apply'에서 'immediately'로 바꾸기전에 alert 띄우기(작성한 사전 질문을 다 지우게됩니다. 계속하시겠습니까?)
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (selectedValue === "apply") setPreQuestionList([]);
+    setSelectedValue((event.target as HTMLInputElement).value);
+  };
+
+  // 사전 질문 input 값 변경 함수
+  const handleSetQuestion = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setPreWriteQuestion(() => e.target.value);
+  };
+
+  // 사전 질문 리스트 등록 함수
+  const handleUpdatePreQuestionList = (
+    e: React.MouseEvent | React.KeyboardEvent<HTMLInputElement>
+  ) => {
+    e.preventDefault();
+
+    if (preQuestionList.length < 5) {
+      let newList = { id: Date.now().toString(), value: preWriteQuestion };
+      setPreQuestionList([...preQuestionList, newList]);
+      setPreWriteQuestion("");
+    }
+  };
+
+  // 사전 질문 리스트 삭제 함수
+  const handleDeleteQuestion = (selectId: string) => {
+    const newPrevQuestionList = preQuestionList.filter(
+      (question) => question.id !== selectId
+    );
+    setPreQuestionList(newPrevQuestionList);
+  };
+
   return (
     <Container maxWidth="lg" sx={{ height: "100%", mb: 10 }}>
-      <Typography variant="h5" fontWeight="bold" textAlign={"center"} my={8}>
+      <Typography variant="h4" fontWeight="bold" textAlign={"center"} my={10}>
         프로젝트/스터디 모집 글 작성
       </Typography>
-      <Box minHeight={300} sx={{ my: 5 }}>
-        <Typography variant="h6" fontWeight="bold">
+      <Box minHeight={350} sx={{ my: 5 }}>
+        <Typography variant="h5" fontWeight="bold">
           기본 정보를 입력해주세요.
         </Typography>
         <Divider
@@ -43,7 +85,7 @@ const PostWritePage = () => {
         />
         <Grid
           container
-          spacing={{ xs: 2, md: 3 }}
+          spacing={{ xs: 3, md: 4 }}
           columns={{ xs: 4, sm: 8, md: 12 }}
         >
           <Grid xs={4} sm={4} md={4}>
@@ -97,23 +139,16 @@ const PostWritePage = () => {
         </Grid>
       </Box>
       <Box>
-        <Typography variant="h6" fontWeight="bold">
+        <Typography variant="h5" fontWeight="bold">
           프로젝트/스터디 진행방식 등 자세한 내용을 적어주세요.
         </Typography>
         <Divider
           orientation="horizontal"
           variant="fullWidth"
           component="hr"
-          sx={{ border: "0.12rem solid #E0CCBE", mt: "0.5rem", mb: "2rem" }}
+          sx={{ border: "0.12rem solid #E0CCBE", mt: "0.5rem" }}
         />
-        {/* 
-        글 작성 시 때 팀원들을 어떻게 받을 지 물어보는 체크 박스
-        1. 즉시참여 받기 checkbox 선택
-        2. 참여신청 받기 checkbox 클릭 시 
-            1) 전달 메세지 (default)  
-            2) 사전 질문 추가 입력 버튼 클릭시 입력 창 제공
-         */}
-        <Box maxWidth="lg" mb={4}>
+        <Box maxWidth="lg" my={4}>
           <Typography variant="subtitle1" fontWeight={"bold"}>
             제목
           </Typography>
@@ -126,17 +161,154 @@ const PostWritePage = () => {
           <QuillEditor value="test" onChange={() => console.log()} />
         </Box>
         <Box>
-          <Typography variant="subtitle2" fontWeight={"bold"}>
+          <Typography variant="subtitle1" sx={{ fontWeight: 700 }}>
             팀원 모집시 원하는 참여방식을 선택해주세요.
           </Typography>
-          <Checkbox {...label} />
-          <span>즉시 참여</span>
-          &nbsp; &nbsp; &nbsp;&nbsp;
-          {/* 참여 신청 버튼 누를 경우, 글 작성자가 팀원들에게 묻고싶은 사전 질문들을 적을 수 있음
-          -> 상세 페이지에서 글 읽는 사람들이 참여신청 누를 시 해당 질문들에 대한 답변을 작성해야 됨 */}
-          <Checkbox {...label} />
-          <span>참여신청</span>
+          <Box
+            sx={{
+              display: "flex",
+              alignItems: "center",
+              color: "grey",
+            }}
+          >
+            <NavigateNextRoundedIcon />
+            <p style={{ fontSize: "0.85rem" }}>
+              <strong>즉시 참여가능</strong> : 모집 글 상세 페이지에서 참여신청
+              버튼을 누른 사람이&nbsp;
+              <em style={{ textDecoration: "underline" }}>바로 그룹에 초대</em>
+              &nbsp;됩니다.
+            </p>
+          </Box>
+          <Box sx={{ display: "flex", alignItems: "center", color: "grey" }}>
+            <NavigateNextRoundedIcon />
+            <p style={{ fontSize: "0.85rem" }}>
+              <strong>참여 신청 받기</strong> : 모집 글 상세 페이지에서 참여신청
+              버튼을 누른 사람은&nbsp;
+              <em style={{ textDecoration: "underline" }}>
+                사전 질문에 대한 답변 작성과 글쓴이의 확인 여부에 따라 그룹에
+                초대
+              </em>
+              &nbsp;됩니다.
+            </p>
+          </Box>
+          <Box
+            sx={{ fontSize: "0.9rem", fontWeight: "bold", color: "#35374B" }}
+          >
+            <label
+              htmlFor="immediately"
+              style={{ paddingLeft: "2rem", cursor: "pointer" }}
+            >
+              즉시 참여 가능
+            </label>
+            <Radio
+              checked={selectedValue === "immediately"}
+              onChange={handleChange}
+              value="immediately"
+              id="immediately"
+              inputProps={{ "aria-label": "Immediately" }}
+              sx={{ mr: 4 }}
+            />
+            <label htmlFor="apply" style={{ cursor: "pointer" }}>
+              참여 신청 받기
+            </label>
+            <Radio
+              checked={selectedValue === "apply"}
+              onChange={handleChange}
+              value="apply"
+              id="apply"
+              inputProps={{ "aria-label": "Apply" }}
+            />
+          </Box>
+          <Box mt={5}>
+            {selectedValue === "apply" && (
+              <>
+                <Typography variant="subtitle1" sx={{ fontWeight: 700 }}>
+                  참여에 관련된 사전 질문 목록을 작성할 수 있습니다.
+                </Typography>
+                <Typography variant="subtitle2" color={"GrayText"}>
+                  사전 질문은 100자 이하로 등록 가능합니다.
+                </Typography>
+              </>
+            )}
+            {selectedValue === "apply" && (
+              <Box mt={2} sx={{ display: "flex", alignItems: "center" }}>
+                {/* TODO: Input keyboard 'Enter' 입력시 등록 처리 */}
+                <Input
+                  color="primary"
+                  variant="soft"
+                  placeholder={
+                    preQuestionList.length < 5
+                      ? "공통적으로 물어보고싶은 기본 질문을 입력해주세요."
+                      : "사전 질문은 5개까지만 등록 가능합니다."
+                  }
+                  sx={{
+                    width: "50vw",
+                    height: "3rem",
+                    marginRight: "2rem",
+                  }}
+                  value={preWriteQuestion}
+                  onChange={(e) => handleSetQuestion(e)}
+                  disabled={!(preQuestionList.length < 5)}
+                />
+                {preWriteQuestion.length > 0 &&
+                  preWriteQuestion.length <= 100 && (
+                    <Tooltip title="사전 질문 추가하기">
+                      <Fab color="primary" aria-label="add" size="small">
+                        <AddIcon
+                          onClick={(e) => handleUpdatePreQuestionList(e)}
+                        />
+                      </Fab>
+                    </Tooltip>
+                  )}
+              </Box>
+            )}
+          </Box>
+          {preQuestionList.length ? (
+            <Box mt={10}>
+              <Typography variant="subtitle1" fontWeight={"bold"}>
+                등록된 사전질문 목록입니다.
+              </Typography>
+              <ul style={{ listStyle: "none" }}>
+                {preQuestionList?.map((list, idx) => (
+                  <li
+                    key={idx}
+                    style={{ display: "flex", alignItems: "center" }}
+                  >
+                    <CheckRoundedIcon sx={{ fontSize: "0.7rem" }} />
+                    <Typography
+                      variant="subtitle1"
+                      sx={{
+                        display: "inline-block",
+                        pl: "0.5rem",
+                        minWidth: "500px",
+                        my: "0.3rem",
+                        color: "#727070",
+                        fontWeight: "bold",
+                      }}
+                    >
+                      {list.value}
+                    </Typography>
+                    <Button
+                      onClick={() => handleDeleteQuestion(list.id)}
+                      variant="outlined"
+                      startIcon={<DeleteIcon />}
+                      size="small"
+                      color="secondary"
+                    >
+                      Delete
+                    </Button>
+                  </li>
+                ))}
+              </ul>
+            </Box>
+          ) : null}
         </Box>
+      </Box>
+      <Box sx={{ marginTop: "3rem", display: "flex", justifyContent: "end" }}>
+        <Button variant="outlined" sx={{ marginRight: "1rem" }}>
+          이전 페이지로
+        </Button>
+        <Button variant="contained">글 등록</Button>
       </Box>
     </Container>
   );
